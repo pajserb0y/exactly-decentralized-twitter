2605aef19c0fd7afac025a7ad38018c0e317018b
pajserb0y <65039799+pajserb0y@users.noreply.github.com>
2022-08-16 22:23:26 +0200

    deployment script plus test for minting and posting

---
+++ deploy/deploy.js
@@ -0,0 +1,24 @@
+const { deployments, network } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deployer } = await getNamedAccounts()
+    const { log, deploy } = deployments
+    const args = []
+
+    const exactly = await deploy("Exactly", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(exactly.address, args)
+    }
+    log("-------------------------------------------")
+}
+
+module.exports.tags = ["exactly", "all"]
--- test/Exactly.test.js
+++ test/Exactly.test.js
@@ -1,126 +1,66 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const { expect } = require("chai");
+const { expect, assert } = require("chai")
+const { deployments, ethers } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+!developmentChains
+    ? describe.skip
+    : describe("Exactly Unit tests", function () {
+          let exactly,
+              deployer,
+              user,
+              URI = "someUri"
+          beforeEach(async () => {
+              await deployments.fixture(["exactly"])
+              exactly = await ethers.getContract("Exactly")
+              const accounts = await ethers.getSigners()
+              deployer = accounts[0]
+              user = accounts[1]
+          })
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+          describe("Deployment", function () {
+              it("Should set the right starting value of state variables", async function () {
+                  expect((await exactly.getTokenCount()).toString()).to.equal("0")
+              })
+          })
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+          describe("Minting profile pictureNFT", function () {
+              it("Should track each minted NFT", async function () {
+                  await exactly.mint(URI)
+                  expect(await exactly.tokenURI(1)).to.equal(URI)
+                  expect((await exactly.getTokenCount()).toString()).to.equal("1")
+              })
+          })
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+          describe("Setting profile picture", function () {
+              it("Should let user select which NFT is their profile picture", async function () {
+                  await exactly.mint(URI)
+                  assert.equal(
+                      (await exactly.getTokenIdForProfile(deployer.address)).toString(),
+                      "1"
+                  )
+              })
+              it("Shouldn't  let user select NFT which is not their own", async function () {
+                  await exactly.mint(URI)
+                  await exactly.connect(user).mint(URI)
+                  await expect(exactly.connect(deployer).setProfile(2)).to.revertedWith(
+                      "Exactly__NotOwner"
+                  )
+              })
+          })
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
+          describe("Tipping posts", function () {
+              it("Should allow users to tip posts", async function () {})
+          })
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+          describe("Withdrawals", function () {
+              describe("Validations", function () {
+                  it("Should revert with the right error if called too soon", async function () {})
 
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
+                  it("Should revert with the right error if called from another account", async function () {})
 
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+                  it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {})
+              })
 
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
-  });
-
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
-
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
-
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
-    });
-
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
-  });
-});
+              describe("Events", function () {})
+          })
+      })
