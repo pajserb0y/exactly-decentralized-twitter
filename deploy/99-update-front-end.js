0d94f47589c0c1933b4ed34e0a695b8529ff2025
pajserb0y <65039799+pajserb0y@users.noreply.github.com>
2022-08-17 19:54:36 +0200

    upload frontend script

---
+++ deploy/99-update-front-end.js
@@ -0,0 +1,39 @@
+const { ethers, network } = require("hardhat")
+const fs = require("fs")
+
+const FRONT_END_ADDRESSES_FILE =
+    "../exactly-decentralized-twitter-frontend/contractData/contractAddresses.json"
+const FRONT_END_ABI_FILE = "../exactly-decentralized-twitter-frontend/contractData/abi.json"
+
+module.exports = async function () {
+    if (process.env.UPDATE_FRONT_END) {
+        console.log("Updating front end...")
+        await updateContractAdresses()
+        await updateAbi()
+    }
+}
+
+async function updateContractAdresses() {
+    const exactly = await ethers.getContract("Exactly")
+    const currentAdresses = JSON.parse(fs.readFileSync(FRONT_END_ADDRESSES_FILE, "utf8"))
+
+    const chainId = network.config.chainId.toString()
+
+    if (chainId in currentAdresses) {
+        if (!currentAdresses[chainId].includes(exactly.address)) {
+            currentAdresses[chainId].push(exactly.address)
+        }
+    } else {
+        currentAdresses[chainId] = [exactly.address]
+    }
+    fs.writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(currentAdresses))
+    console.log("Written address")
+}
+
+async function updateAbi() {
+    const exactly = await ethers.getContract("Exactly")
+    fs.writeFileSync(FRONT_END_ABI_FILE, exactly.interface.format(ethers.utils.FormatTypes.json))
+    console.log("Written abi")
+}
+
+module.exports.tags = ["all", "frontend"]
